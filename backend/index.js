ca4a8f267c3dbc5c8494ec9805b265b25a37aa3f
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-03-09 22:52:49 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(express.json());
+mongoose.connect(  //to connect to our mongodb server
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("Connected to MongoDB");
+    }
+  );
 
 app.listen(8800,()=>{     
     console.log("Backend server kam kar rha hai");
