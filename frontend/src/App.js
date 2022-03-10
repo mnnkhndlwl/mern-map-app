d203345b6ec48c0becb611b2d87c125d2f3b2faa
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-03-10 18:23:18 +0530

    finally map is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import * as React from 'react';
-import Map from 'react-map-gl';
+import  ReactMapGL , { Marker, Popup } from "react-map-gl";
+import {Room} from '@material-ui/icons';
+import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
 
 function App() {
-  return(
-    <div className="App">
-     <Map
-    initialViewState={{
-      longitude: -100,
-      latitude: 40,
-      zoom: 3.5
-    }}
+  const [viewState, setViewState] = React.useState({
+    
+    zoom: 3.5
+  });
+  return (
+
+    <ReactMapGL
     style={{width: '100vw', height: '100vh'}}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
+        {...viewState}
+    onMove={evt => setViewState(evt.viewState)}
+       mapStyle= "mapbox://styles/grx406/cl0kzfg1t008014qbgzdhd5xj"
     mapboxAccessToken= {process.env.REACT_APP_MAPAPI}
-  />
-  </div>
+      >
+        <Marker longitude={2.29449905431968} latitude={48.8582602} offsetLeft={-20} offsetTop={-10}>
+      <Room style={{fontSize:viewState.zoom*10}}/>
+    </Marker>
+      </ReactMapGL>
+    
   );
 }
 export default App;
